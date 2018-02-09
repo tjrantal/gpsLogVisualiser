5a2696606af53222f28de390106ed528967246e9
Timo Rantalainen <tjrantal@gmail.com>
2018-02-09 07:11:06 +0200

    Added creating plots with java, and changed the java build system from ant to gradle

--- visualiseGPS.m
+++ visualiseGPS.m
@@ -8,23 +8,27 @@ close all;
 clear all;
 clc;
 
+%if exist('OCTAVE_VERSION', 'builtin') > 0
+%	graphics_toolkit('gnuplot');	%Try to use the gnuplot graphics toolkit. FLTK seg-faulted
+%end
+
+if exist('OCTAVE_VERSION', 'builtin') > 0
+	pkg load image
+end
 addpath('functions');
 addpath('functions/simplify');
-javaaddpath('java/build/GoogleMapsApiHelper.jar');	%Java helper to interface with Google maps and elevations APIs
+javaaddpath('build/libs/runAnalyserHelper-1.0.jar');	%Java helper to interface with Google maps and elevations APIs. Saves png images as well to comply with Google Elevations API terms of service
 interestingColumns = {'lat','lon'};	%Column headers for latitude and longitude, respectively
 dataPath = 'sampleData/gpsLog';	%reads logged coordinate data from this path
-savePath = 'mapFigs';		%Saves google static map figures here
 elevationsPath = 'elevationFigs';	%Saves google static map and elevations visualisation here
 elevationsSavePath = 'sampleData/googleElevations';	%Saves the google elevation to a text file here
 keyFile = 'mapsKey.txt';	%Google static maps API key in a text file
 elevationsKeyFile = 'elevationsKey.txt';	%Google elevations API key in a text file
 mapsKey = strtrim(fileread(keyFile));
 elevationsKey = strtrim(fileread(elevationsKeyFile));
-%Create folder for figures if required
-if ~exist(savePath,'dir')
-	mkdir(savePath)
-end
+elevationRequest = javaObject('timo.home.elevations.ElevationRequest',elevationsKey);
 
+%Create folder for text files and figures if required
 if ~exist(elevationsPath,'dir')
 	mkdir(elevationsPath)
 end
@@ -38,19 +42,37 @@ fList = dir([dataPath '/*.txt']);
 for file = {fList(:).name}
 	try
 		%Fetch only the ones that have not yet been fetched
-		if ~exist([savePath '/' strrep(file{1},'.txt','.png')],'file')
+		if ~exist([elevationsPath '/' strrep(file{1},'.txt','.png')],'file')
 			%Read data	
 			data = readLog([dataPath '/' file{1}]);
 			%Get latitude and longitude column indices
 			columnIndices = cellfun(@(x) getIndice(data.header(1).line,x),interestingColumns);
+			
+			%Simplify coordinates
+			[simplified, tIndices] = simplifyJava([data.data(:,columnIndices)], 1e-4);
+			
+			%Encode coordinates with polyencode
+			encodedString = javaMethod('encode','timo.home.polyencode.PolyEncode',simplified(:,1),simplified(:,2));
 
+			%Send data off to Google static maps api, and get the static map as java BufferedImage
+			%urlwrite(['https://maps.googleapis.com/maps/api/staticmap?size=640x640&path=weight:3%7Ccolor:red%7Cenc:' encodedString '&key=' mapsKey],[savePath '/' strrep(file{1},'.txt','.png')]);
+			mapString = urlread(['https://maps.googleapis.com/maps/api/staticmap?size=640x640&path=weight:3%7Ccolor:red%7Cenc:' encodedString '&key=' mapsKey]);
+			
+			%Get the map as bufferedImage
+			iStream = javaObject('java.io.ByteArrayInputStream',typecast(mapString,'int8'));
+			mapBI = javaMethod('read','javax.imageio.ImageIO',iStream);
+			
 			%Get elevations
-			elevationsArray = javaObject('timo.home.elevations.ElevationRequest',data.data(:,columnIndices(1)),data.data(:,columnIndices(2)),elevationsKey);
-			elevations = elevationsArray.getElevations();
-			resolution = elevationsArray.getResolution();
-			%lat = elevationsArray.getLatitudes();
-			%lon = elevationsArray.getLongitudes();
-			clear 'elevationsArray';
+			elevationRequest.encode(data.data(:,columnIndices(1)),data.data(:,columnIndices(2)));
+			elevations = elevationRequest.getElevations();
+			resolution = elevationRequest.getResolution();
+			%lat = elevationRequest.getLatitudes();
+			%lon = elevationRequest.getLongitudes();
+
+			%Save a combined static map and elevations png
+			xChart = javaObject('timo.jyu.PlotXChart','Elevations from Google Elevations API','GPS Data Point Index','Elevation [m]',640,320);
+			xChart.addSeries('Elevation',1:length(elevations),elevations);
+			xChart.appendAndSavePNG([elevationsPath '/' strrep(file{1},'.txt','.png')],mapBI,true);
 			
 			%Write the elevations to a file
 			eleSaveName = [elevationsSavePath '/' strrep(file{1},'GPS_','ELE_')];
@@ -69,33 +91,8 @@ for file = {fList(:).name}
 			%write data to end of file
 			dlmwrite(eleSaveName,[data.data, elevations, resolution],'\t','-append');
 			
-			%Simplify coordinates
-			[simplified, tIndices] = simplifyJava([data.data(:,columnIndices)], 1e-4);
-			%Encode coordinates with polyencode
-			encodedString = javaMethod('encode','timo.home.polyencode.PolyEncode',simplified(:,1),simplified(:,2));
-
-			%Send data off to Google static maps api
-			urlwrite(['https://maps.googleapis.com/maps/api/staticmap?size=640x640&path=weight:3%7Ccolor:red%7Cenc:' encodedString '&key=' mapsKey],[savePath '/' strrep(file{1},'.txt','.png')]);
-			
-			%Produce a combined file elevations and map
-			[tempData, mapping] = imread([savePath '/' strrep(file{1},'.txt','.png')]);
-			imData = ind2rgb(tempData,mapping);
-			fh = figure('position',[10 10 1024 1365]);
-			subplot(4,1,[1:3]);
-			imshow(imData);
-			title([strrep(file{1},'.txt','.png') ' from Google Static Maps']);
-			subplot(4,1,4);
-			plot(elevations,'k','linewidth',2);
-			title('Elevations from Google Elevations API');
-			ylabel('[m]')
-			xlabel('Coordinate index');
-			if exist('OCTAVE_VERSION', 'builtin') > 0
-				print(fh,[elevationsPath '/' strrep(file{1},'.txt','.png')]);
-			else
-				print('-dpng','-r200',[elevationsPath '/' strrep(file{1},'.txt','.png')]);			
-			end
-			close(fh);
 			disp(['fetched ' file{1}]);
+			break;
 		else
 			disp(['EXISTS ' file{1}]);
 		end
