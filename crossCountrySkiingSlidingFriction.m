121ed968dba80a08131c68c2bdefd2ad831e3918
Timo Rantalainen <tjrantal@gmail.com>
2018-04-14 15:14:27 +0300

    Removed debugging code

--- crossCountrySkiingSlidingFriction.m
+++ crossCountrySkiingSlidingFriction.m
@@ -56,106 +56,19 @@ eleIndices = find(eleStamps >= epochStamps(1) & eleStamps <= epochStamps(2));
 velocity = eleData.data(eleIndices,eleColumnIndices(4));
 smoothVel = filtfilt(b,a,velocity);
 
-%Find downhill with no pedaling by exploring the data manually
-ah = [];
-figure
-subplot(2,1,1)
-plot((eleStamps(eleIndices)-eleStamps(eleIndices(1)))./(1000),eleData.data(eleIndices,eleColumnIndices(5)),'linewidth',3);
-ah(1) = gca();
-subplot(2,1,2)
-plot((eleStamps(eleIndices)-eleStamps(eleIndices(1)))./(1000),velocity,'linewidth',3);
-hold on
-plot((eleStamps(eleIndices)-eleStamps(eleIndices(1)))./(1000),smoothVel,'g','linewidth',3);
-
-
-ah(2) = gca();
-
-linkaxes(ah,'x');
-
-%Plot coordinates
-%figure
-%plot(eleData.data(eleIndices,eleColumnIndices(2)),eleData.data(eleIndices,eleColumnIndices(3)),'linewidth',3)
-
-%Plot distance
-distance = calcDistance(eleData.data(eleIndices,eleColumnIndices(2)),eleData.data(eleIndices,eleColumnIndices(3)));
-
-%Filter distance
-
-distanceSmooth = filtfilt(b,a,distance);
-
-
 
 distanceVel = eleData.data(eleIndices(2:end),eleColumnIndices(4)).*diff((eleStamps(eleIndices)-eleStamps(eleIndices(1)))./(1000));
 distanceSmoothVel = smoothVel(2:end).*diff((eleStamps(eleIndices)-eleStamps(eleIndices(1)))./(1000));
-
-dposition =cumtrapz(distance);
-dsposition = cumtrapz(distanceSmooth);
-velposition =cumtrapz(distanceVel);
 smoothvelposition = cumtrapz(distanceSmoothVel);
-
-
-figure
-plot(dposition,'linewidth',3);
-hold on;
-plot(velposition,'linewidth',3,'r');
-plot(dposition,'linewidth',3,'g');
-plot(smoothvelposition,'linewidth',3,'k')
-title('Position')
-figure
-plot(distance,'linewidth',3);
-hold on;
-plot(distanceVel,'linewidth',3,'r');
-plot(distanceSmooth,'linewidth',3,'g');
-title('diffPosition')
-%keyboard;
 
 heightSmooth = filtfilt(b,a,eleData.data(eleIndices,eleColumnIndices(5)));
 
-figure
-plot(eleData.data(eleIndices,eleColumnIndices(5)),'linewidth',3);
-hold on;
-plot(heightSmooth,'linewidth',3,'g');
-title('heightData');
-
-figure
-plot(diff(eleData.data(eleIndices,eleColumnIndices(5))),'linewidth',3);
-hold on;
-plot(diff(heightSmooth),'linewidth',3,'g');
-title('diffHeightData');
-
 %Calc slope profile based on velocity and elevation change
-drop = diff(eleData.data(eleIndices,eleColumnIndices(5)));
+
 dropSmooth = diff(heightSmooth);
 distanceVel = eleData.data(eleIndices(1:end-1),eleColumnIndices(4)).*diff(eleStamps(eleIndices-1)./1000);
-figure
-subplot(2,1,1);
-plot(drop,'linewidth',3);
-hold on
-plot(dropSmooth,'linewidth',3,'g');
-title('Drop');
-subplot(2,1,2);
-plot(distanceVel,'linewidth',3);
-hold on
-plot(distance,'linewidth',3);
-plot(distanceSmooth,'linewidth',3,'g');
-slopeAngle = asin(drop./distanceVel);
-slopeAngle2 = asin(drop./distance);
-slopeAngleSmooth = asin(dropSmooth./distanceSmooth);
+
 slopeAngleSmoothVel = asin(dropSmooth./distanceSmoothVel);
-
-
-figure
-plot(slopeAngle/pi*180,'linewidth',3);
-hold on;
-plot(slopeAngle2/pi*180,'linewidth',3);
-plot(slopeAngleSmooth/pi*180,'linewidth',3,'g');
-title('slopeAngle');
-
-
-%[b,a] = butter(2,0.2/0.5);
-%slopeAngleSmooth = filtfilt(b,a,slopeAngle2);
-%plot(slopeAngleSmooth/pi*180,'linewidth',3,'g');
-%keyboard;
 
 %Use distance-based slope, which is slopeAngle2
 %Model velocity against measured velocity
@@ -170,7 +83,7 @@ constants.g = -9.81;	%m/s2 gravitational acceleration
 constants.u = 0.02;
 constants.Cd = 1.0;	%Guess base on cyclists https://www.cyclingpowerlab.com/CyclingAerodynamics.aspx
 %constants.Cd = 0.1;	%Guess base on cyclists https://www.cyclingpowerlab.com/CyclingAerodynamics.aspx
-constants.Cd = 0.0;	%Guess base on cyclists https://www.cyclingpowerlab.com/CyclingAerodynamics.aspx
+%constants.Cd = 0.0;	%Guess base on cyclists https://www.cyclingpowerlab.com/CyclingAerodynamics.aspx
 
 constants.time = (eleStamps(eleIndices)-eleStamps(eleIndices(1)))./(1000);
 
